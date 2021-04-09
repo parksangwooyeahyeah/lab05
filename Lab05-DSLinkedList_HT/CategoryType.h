6428f29281fde2f0811f94ff19ab3799ca843c67
parksangwooyeahyeah <parksangwooyeahyeah@gmail.com>
2021-04-09 10:49:34 +0900

    FRI 10:49

--- Lab05-DSLinkedList_HT/CategoryType.h
+++ Lab05-DSLinkedList_HT/CategoryType.h
@@ -1,6 +1,7 @@
 #include "DoublySortedLinkedList.h"
 #include "List_Sorted_Array.h"
 #include "SimpleProductType.h"
+#include "List_Sorted_SingleLink.h"
 
 class CategoryType
 {
@@ -14,6 +15,8 @@ public:
 
 private:
     int itemNum;
-    DoublySortedLinkedList<SimpleProductType> cateList;
+    List_Sorted_Array<SimpleProductType> cateList;
+    List_Sorted_Array<simpleproduct type> subcategory;
+    List_Sorted_SingleLink<simpleproduct type> subsubcategory;
     List_Sorted_Array<ItemType> m_list; //마스터리스트는 이미 애플리케이션에서 존재하는걸 가져와야하는데 어떻게 가져오지?
 };
